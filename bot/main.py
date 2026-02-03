a1a8507ab729e2150ac00c3efe171ecf922f3645
animestopbus-ctrl <animestopbus@gmail.com>
2026-02-03 08:49:08 +0530

    Update main.py

--- bot/main.py
+++ bot/main.py
@@ -6,11 +6,15 @@ from config import API_ID, API_HASH, BOT_TOKEN
 
 from utils.logger import logger
 
-import handlers.start
-import handlers.search
-import handlers.pagination
+from handlers.start import register_handlers as register_start
+from handlers.search import register_handlers as register_search
+from handlers.pagination import register_handlers as register_pagination
 
 client = Client("discovery_bot", api_id=API_ID, api_hash=API_HASH, bot_token=BOT_TOKEN)
+
+register_start(client)
+register_search(client)
+register_pagination(client)
 
 async def main():
     logger.info("Starting bot...")
